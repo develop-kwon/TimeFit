4f33155fb12688be1fa9d18b970fcf55a82d329d
YuHyun <kyh010316@naver.com>
2025-12-16 15:11:04 +0900

    main 수정

--- src/main.jsx
+++ src/main.jsx
@@ -4,16 +4,7 @@ import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
 
 createRoot(document.getElementById('root')).render(
-<<<<<<< Updated upstream
-    <App />
-<<<<<<< HEAD
-)
-=======
     <BrowserRouter>
         <App />
     </BrowserRouter>
 )
->>>>>>> Stashed changes
-=======
-)
->>>>>>> e3d1acc91fd89edf91c87b241886311f875b2785
